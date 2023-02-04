10625412e31dff0e3bbfb1529463144209beecdf
PatriciaIvee <patriciaiveeleones@gmail.com>
2023-02-04 08:55:19 +0800

    February 3, 2023 Lecture Module save_images How to Create A drawingBoard on App
    Creating Drawing App with TouchEventView.kt

---
+++ save_images/src/main/java/ph/kodego/leones/patricia/ivee/save_images/DatabaseHandler.kt
@@ -0,0 +1,59 @@
+package ph.kodego.leones.patricia.ivee.save_images
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHandler(context: Context): SQLiteOpenHelper(context,DATABASENAME,null,DATABASEVERSION) {
+//Android Studio uses SQLite
+//    DATABASENAME = name of database
+//    Database is collection of Schemas and Tables
+//    When you change or added a new table anything you do in android you must change DATABASEVERSION
+    companion object {
+        private val DATABASEVERSION = 1
+        private val DATABASENAME = "imagedatabase"
+
+        val TABLE_IMAGES = "table_images"
+        val TABLE_IMAGES_ID = "id"
+        val TABLE_IMAGES_DATA = "data"
+
+        val TABLE_IMAGES_TEXT = "table_images"
+        val TABLE_IMAGES_TESXT_ID = "id"
+        val TABLE_IMAGES_TEXT_DATA = "data"
+
+    }
+
+//    Everytime you install on Create 1st time app runs
+    override fun onCreate(db: SQLiteDatabase?) {
+        val CREATE_TABLE_IMAGES =
+                "CREATE TABLE $TABLE_IMAGES " +
+                "($TABLE_IMAGES_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "$TABLE_IMAGES_DATA BLOB)"
+//    blob is binary large object saves image as is converts image to binary
+        db?.execSQL(CREATE_TABLE_IMAGES)
+
+        val CREATE_TABLE_IMAGES_TEXT =
+            "CREATE TABLE $TABLE_IMAGES_TEXT " +
+                    "($TABLE_IMAGES_TESXT_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "$TABLE_IMAGES_TEXT_DATA TEXT)"
+        db?.execSQL(CREATE_TABLE_IMAGES_TEXT)
+
+//    Difference of these two is when you save image as a text it converts image to text something readable for humans
+//    Text Image is also memory efficient meaning you can save more memory with textImage
+//    blob converts to binary code also more memory usage
+//    Both of these are highly used to save images in the database
+//
+}
+//Primary Key is the UNIQUE IDENTIFIER
+//    AUTOINCREMENT automatically sets Primary Key (only works for numerical Primary Key values)
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db!!.execSQL("DROP TABLE IF EXISTS $TABLE_IMAGES")
+        db!!.execSQL("DROP TABLE IF EXISTS $TABLE_IMAGES_TEXT")
+
+        onCreate(db)
+    }
+
+
+
+}
---
+++ save_images/src/main/java/ph/kodego/leones/patricia/ivee/save_images/TouchEventView.kt
@@ -0,0 +1,205 @@
+package ph.kodego.leones.patricia.ivee.save_images
+
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.*
+import android.graphics.Bitmap.CompressFormat
+import android.graphics.Bitmap.createBitmap
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.view.ViewConfiguration
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.content.res.ResourcesCompat
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.*
+import kotlin.math.ceil
+
+
+private const val STROKE_WIDTH = 12f
+
+class TouchEventView (context:Context,attrs: AttributeSet):AppCompatImageView(context, attrs) {
+
+
+    private val path = Path()
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+    private lateinit var extraCanvas:Canvas
+    private lateinit var extraBitmap: Bitmap
+    private lateinit var frame: Rect
+    private var viewWidth: Int = 0
+    private var viewHeight: Int = 0
+
+    private val paint = Paint().apply{
+        color = drawColor
+        isAntiAlias = true
+        isDither = true
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+        strokeWidth = STROKE_WIDTH
+    }
+
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+    private var currentX = 0f
+    private var currentY = 0f
+    private var motionTouchEventX= 0f
+    private var motionTouchEventY = 0f
+
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        extraBitmap = createBitmap(width,height,Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+        viewHeight = height
+        viewWidth = width
+
+        val inset = 40
+        frame = Rect(inset, inset, width - inset, height - inset)
+
+    }
+
+    override fun onDraw(canvas: Canvas){
+        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+        //Add Frame
+//        extraCanvas.drawRect(frame, paint)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        motionTouchEventX = event.x
+        motionTouchEventY = event.y
+
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> touchStart()
+            MotionEvent.ACTION_MOVE -> touchMove()
+            MotionEvent.ACTION_UP -> touchUp()
+            else -> Log.i("EVENT", event.action.toString())
+
+        }
+        return true
+    }
+
+    private fun touchStart(){
+        path.reset()
+        path.moveTo(motionTouchEventX,motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+    }
+
+    private fun touchMove(){
+        val dx = Math.abs(motionTouchEventX - currentX)
+        val dy = Math.abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance){
+            path.quadTo(currentX, currentY, (motionTouchEventX + currentX)/ 2, (motionTouchEventY +currentY)/2)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+            extraCanvas.drawPath(path, paint)
+        }
+        invalidate()
+    }
+
+    private fun touchUp(){
+        path.reset()
+    }
+
+    fun loadImage(){
+        val externalFolder = "${context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)}${File.separator}sample_images"
+        var imageFile = File(externalFolder, "sample(1).jpg")
+        var newBitmap :Bitmap? = null
+
+        var fileInputStream = FileInputStream(imageFile)
+        val bmpFactoryOptions = BitmapFactory.Options()
+        bmpFactoryOptions.inJustDecodeBounds = true
+
+        val widthRatio =
+            ceil((bmpFactoryOptions.outWidth / viewWidth).toDouble()).toInt()
+        val heightRatio =
+            ceil((bmpFactoryOptions.outHeight / viewHeight).toDouble()).toInt()
+
+        if (heightRatio>widthRatio) {
+            bmpFactoryOptions.inSampleSize = heightRatio
+        } else{
+            bmpFactoryOptions.inSampleSize = widthRatio
+        }
+        bmpFactoryOptions.inJustDecodeBounds = false
+        newBitmap = BitmapFactory.decodeStream(fileInputStream, null, bmpFactoryOptions)
+
+        path.reset()
+        extraBitmap = createBitmap(newBitmap!!.width, newBitmap!!.height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawBitmap(newBitmap!!, 0f, 0f, paint)
+        invalidate()
+    }
+
+    fun loadImage(uri: Uri){
+        var newBitmap:Bitmap? = null
+    }
+
+
+    fun saveImage(){
+
+        val date = Date()
+        val fileInfo = "${date.month}${date.date}${date.year}${date.hours}${date.minutes}"
+
+        val externalFolder = "${context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)}${File.separator}sample_images"
+        if(!File(externalFolder).exists()){
+            File(externalFolder).mkdir()
+            Log.i("FILES", "Directory created : ${externalFolder}")
+
+        }
+
+        val saveFile:File = File(externalFolder, "KODEGO_IMAGE-${fileInfo}.jpg")
+
+        try {
+            val fileOutputStream = FileOutputStream(saveFile)
+
+            extraBitmap.compress(CompressFormat.JPEG, 95, fileOutputStream)
+            fileOutputStream.flush()
+            fileOutputStream.close()
+        } catch (e:Exception){
+            Log.e("ERROR", "Saving File Error")
+            Log.e("ERROR", e.message.toString())
+
+        }
+
+    }
+
+   fun saveImageByMediaStore(name:String){
+       val values = ContentValues().apply {
+           put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+           put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+           put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM)
+
+       }
+
+       val resolver = context.contentResolver
+       var uri:Uri? = null
+
+       try {
+           uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values)
+               ?:throw IOException("Failed to create a MediaStore Record.")
+
+           resolver.openOutputStream(uri)?.use{
+               if (!extraBitmap.compress(CompressFormat.JPEG,95,it))
+                   throw IOException("Failed save bitmap.")
+           } ?: throw IOException ("Failed to open Output Stream.")
+       } catch (e:IOException){
+           uri?.let { orphanUri ->
+               resolver.delete(orphanUri, null, null)
+
+           }
+                e.printStackTrace()
+       }
+
+   }
+
+}
